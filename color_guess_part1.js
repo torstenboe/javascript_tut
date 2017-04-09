0acadc8008e40bc89b7849247518c61262b36f82
Torsten <tboettjer@live.de>
2017-04-09 11:57:04 +0200

    input

--- color_guess_part1.js
+++ color_guess_part1.js
@@ -1,5 +1,9 @@
 var colors = ['Blue', 'Green', 'Gray', 'Pink', 'Red', 'Yellow', 'White', 'Black']
-
+var target;
+var guess_input_text;
+var guess_input;
+var finished = false;
+var guesses = 0;
 
 function do_game() {
   var random_number = Math.random() * 8;
@@ -7,4 +11,35 @@ function do_game() {
   target = random_number_integer;
   colors = colors.sort();
   alert(colors + "\n" + target + "\n" + "Target is color: " + colors[target]);
-}
+
+  while (!finished) {
+      guess_input_text = prompt("I am thinking of these colors\n\n" + colors +
+                                "\n\n What is your number?");
+      guess_input = parseInt(guess_input_text);
+      guesses += 1;
+      finished = check_guess();
+   }
+
+function check_guess() {
+       if (isNaN(guess_input)) {
+           alert("You have not entered a number.\n\n" +
+                 "Please enter a number in the range 1 to 8.");
+           return false;
+       }
+       if ((guess_input < 1) || (guess_input > 8)) {
+           alert("Please enter an integer number in the range 1 to 8.");
+           return false;
+       }
+       if (guess_input > target) {
+           alert("Your number is too large!");
+           return false;
+       }
+       if (guess_input < target) {
+           alert("Your number is too small!");
+           return false;
+       }
+       alert("You got it! The number was " + target +
+             ".\n\nIt took you " + guesses +
+             " guesses to get the number!");
+       return true;
+   }
