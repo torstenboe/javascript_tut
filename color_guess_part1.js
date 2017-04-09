0b5ec7d84c90892a1466719734e92d61580b3a0e
Torsten <tboettjer@live.de>
2017-04-09 11:39:37 +0200

    sort

--- color_guess_part1.js
+++ color_guess_part1.js
@@ -5,5 +5,6 @@ function do_game() {
   var random_number = Math.random() * 8;
   var random_number_integer = Math.floor(random_number);
   target = random_number_integer;
+  colors = colors.sort();
   alert(colors + "\n" + target + "\n" + "Target is color: " + colors[target]);
 }
