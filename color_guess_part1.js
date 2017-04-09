1214f71bffb91a3a62fc00cf058157461ac845ef
Torsten <tboettjer@live.de>
2017-04-09 11:19:29 +0200

    none

--- color_guess_part1.js
+++ color_guess_part1.js
@@ -2,10 +2,5 @@ var color = ['Blue', 'Green', 'Gray', 'Pink', 'Red', 'Yellow', 'White', 'Black']
 
 
 function do_game() {
-    var random_number = Math.random() * 100;
-    var random_number_integer = Math.floor(random_number);
-    target = random_number_integer;
-
-    while (!finished) {
-      console.console.log("test");
-    }
+  console.log("test");
+}
