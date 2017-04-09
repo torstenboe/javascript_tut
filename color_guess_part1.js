560fa2931ee9a9b7f444256a0d7b1fae9d0882b7
Torsten <tboettjer@live.de>
2017-04-09 11:36:29 +0200

    correct

--- color_guess_part1.js
+++ color_guess_part1.js
@@ -5,5 +5,5 @@ function do_game() {
   var random_number = Math.random() * 8;
   var random_number_integer = Math.floor(random_number);
   target = random_number_integer;
-  alert(colors + target + "Target is color: ", colors[target]);
+  alert(colors + "   " + target + "   " + "Target is color: " + colors[target]);
 }
